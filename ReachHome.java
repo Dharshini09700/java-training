58456dec2750e11a972be5d911571436794d9d8f
Dharshini Muthusamy <e24ai045@shanmugha.edu.in>
2026-09-25 14:35:52 +0530

    Create ReachHome.java

---
+++ ReachHome.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Codechef{
+	public static void main (String[] args) throws java.lang.Exception{
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		for(int i=0;i<a;i++){
+		    int b = sc.nextInt();
+		    int c = sc.nextInt();
+		    if(b*5 >= c){
+		        System.out.println("YES");
+		    }
+		    else{
+		        System.out.println("NO");
+		    }
+		}
+
+	}
+}
